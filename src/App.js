f88e7e5508f078ae3375e3b1a75d3e9f370fd00d
Severin Spoerri <severin.spoerri@outlook.com>
2020-01-05 17:07:19 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello From React App
-      </h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Hello From React App
+        </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
