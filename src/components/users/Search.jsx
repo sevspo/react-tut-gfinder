3f29a07b4bf40618d4f7623984bd9da2635b3008
Severin Spoerri <severin.spoerri@outlook.com>
2020-01-28 00:07:15 +0100

    added routing

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -9,17 +9,22 @@ export default class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired, 
+        setAlert: PropTypes.func.isRequired
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
 
     onSubmit = e => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({
-            text: ''
-        })
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter search term', 'light')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({
+                text: ''
+            })
+        }
     }
 
     render() {
